cb0917e16c320f739a49bd2179bb6be10bdc0683
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 15:50:06 +0100

    add routing back to App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import HeaderContainer from "./components/Header/Header-Container";
 import MainContainer from "./components/Main/Main-Container";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App-Container">
       <HeaderContainer />
-      <MainContainer />
+      <Routes>
+        <Route path="/" element={<MainContainer />} />
+        <Route path="/:topics" element={<MainContainer />} />
+      </Routes>
     </div>
   );
 }
