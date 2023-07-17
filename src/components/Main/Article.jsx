d02062a8d385a22da001d667ce63f148f6ab95b0
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 12:07:09 +0100

    create Article component layout

--- src/components/Main/Article.jsx
+++ src/components/Main/Article.jsx
@@ -1,5 +1,23 @@
-function Article(params) {
-  return <p>Article-List</p>;
+function Article({ articles }) {
+  return (
+    <>
+      {articles.map((article) => {
+        return (
+          <dir clasName="article">
+            <section className="article-header">
+              <h2>{article.title}</h2>
+              <p>{article.author}</p>
+            </section>
+            <img src={article.article_img_url} alt="" />
+            <section className="vote-comment-section">
+              <p>Votes: {article.votes}</p>
+              <p>Comments: {article.comment_count}</p>
+            </section>
+          </dir>
+        );
+      })}
+    </>
+  );
 }
 
 export default Article;
