97cc28e6b8024a59241b8e828156e24e138f97b8
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 11:34:13 +0100

    create basic main layout

---
+++ src/components/Main/Main-Container.jsx
@@ -0,0 +1,11 @@
+import Article from "./Article";
+
+function MainContainer(params) {
+  return (
+    <div className="main-container">
+      <Article />
+    </div>
+  );
+}
+
+export default MainContainer;
