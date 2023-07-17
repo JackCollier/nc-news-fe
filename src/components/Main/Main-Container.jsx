bf62b1c0bfba58b8c3cf4223f7dc1f53f4832646
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 14:51:48 +0100

    add page button functionality

--- src/components/Main/Main-Container.jsx
+++ src/components/Main/Main-Container.jsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import Article from "./Article";
 import { getArticles } from "../utils/Api-Util";
 import { Route, Routes } from "react-router-dom";
+import PageButtons from "./PageButtons";
 
 function MainContainer(params) {
   const [articles, setArticles] = useState([]);
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+  console.log(currentPageNumber);
 
   useEffect(() => {
     getArticles().then((articleData) => {
@@ -12,11 +15,22 @@ function MainContainer(params) {
     });
   }, []);
 
+  const handlePageChange = (binary) => {
+    if (binary) {
+      setCurrentPageNumber(currentPageNumber + 1);
+    } else {
+      setCurrentPageNumber(currentPageNumber - 1);
+    }
+  };
+
   return (
     <div className="main-container">
       <Routes>
         <Route path="/" element={<Article articles={articles} />} />
       </Routes>
+      <section>
+        <PageButtons handlePageChange={handlePageChange} />
+      </section>
     </div>
   );
 }
