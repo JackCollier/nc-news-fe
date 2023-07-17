7b7e2c32aa21d315c96067dac4fd1b1c0a0aa526
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 11:23:31 +0100

    create HeaderContainer componenet

---
+++ src/components/Header/Header-Container.jsx
@@ -0,0 +1,11 @@
+import DropDown from "./Drop-Down";
+import Title from "./Title";
+
+function HeaderContainer() {
+  return (
+    <header>
+      <Title />
+      <DropDown />
+    </header>
+  );
+}
