06d6bbe61c48be992990f4d215e95ab03a9ebe05
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 11:53:57 +0100

    create useEffect function to fetch all articles

--- src/components/Main/Main-Container.jsx
+++ src/components/Main/Main-Container.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 import Article from "./Article";
+import { getArticles } from "../utils/Api-Util";
 
 function MainContainer(params) {
+  useEffect(() => {
+    getArticles().then((articleData) => {
+      console.log(articleData);
+    });
+  }, []);
+
   return (
     <div className="main-container">
       <Article />
