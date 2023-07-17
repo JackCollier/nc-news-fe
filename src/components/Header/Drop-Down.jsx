e2a8af2d6b3335cbb673ce2db5ab9795ff450267
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 15:22:23 +0100

    create logic to fetch topics from api

--- src/components/Header/Drop-Down.jsx
+++ src/components/Header/Drop-Down.jsx
@@ -1,16 +1,22 @@
 import { useState, useEffect } from "react";
+import { getTopics } from "../utils/Api-Util";
 
 function DropDown(params) {
   const [open, setOpen] = useState(false);
+  const [topics, setTopics] = useState([]);
 
   const handleOpen = () => {
     setOpen(!open);
   };
 
+  useEffect(() => {
+    getTopics().then((data) => setTopics(data));
+  }, [open]);
+
   return (
     <>
       <i className="fa fa-bars" aria-hidden="true" onClick={handleOpen}>
-        {open ? <div>open</div> : <div></div>}
+        {open ? <div className="dropdown-box"></div> : <div></div>}
       </i>
     </>
   );
