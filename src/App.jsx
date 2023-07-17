c57f9961501d90e038f40d1b54832c57ddfbec45
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 11:47:10 +0100

    add Routing for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import HeaderContainer from "./components/Header/Header-Container";
 import MainContainer from "./components/Main/Main-Container";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App-Container">
       <HeaderContainer />
-      <MainContainer />
+      <Routes>
+        <Route path="/" element={<MainContainer />} />
+      </Routes>
     </div>
   );
 }
