ab1cb40fe1e6a9bbbd6d1e03d1edc425e633a751
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 15:53:26 +0100

    add topic useParams handling

--- src/components/Main/Main-Container.jsx
+++ src/components/Main/Main-Container.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import Article from "./Article";
 import { getArticles } from "../utils/Api-Util";
-import { Route, Routes } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import PageButtons from "./PageButtons";
 
 function MainContainer(params) {
+  const { topics } = useParams();
   const [articles, setArticles] = useState([]);
   const [currentPageNumber, setCurrentPageNumber] = useState(1);
 
@@ -12,7 +13,7 @@ function MainContainer(params) {
     getArticles(currentPageNumber).then((articleData) => {
       setArticles(articleData);
     });
-  }, [currentPageNumber]);
+  }, [currentPageNumber, topics]);
 
   const handlePageChange = (binary) => {
     binary
@@ -22,9 +23,7 @@ function MainContainer(params) {
 
   return (
     <div className="main-container">
-      <Routes>
-        <Route path="/" element={<Article articles={articles} />} />
-      </Routes>
+      <Article articles={articles} />
       <section className="page-number-container">
         <PageButtons
           handlePageChange={handlePageChange}
