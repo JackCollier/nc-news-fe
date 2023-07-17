9e74e578187086d23614aa785e8c3f66546355e7
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 14:57:03 +0100

    allow endpoint to handle limit and page parameter

--- src/components/utils/Api-Util.js
+++ src/components/utils/Api-Util.js
@@ -4,8 +4,13 @@ const newsApi = axios.create({
   baseURL: "https://nc-news-kwsf.onrender.com/api",
 });
 
-export const getArticles = () => {
-  return newsApi.get("/articles").then((res) => {
+export const getArticles = (pageNum) => {
+  const params = {
+    limit: 5,
+    page: pageNum,
+  };
+
+  return newsApi.get("/articles", { params }).then((res) => {
     return res.data.articles.articles;
   });
 };
