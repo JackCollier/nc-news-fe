cafcb033dcb2a976ccd5d2cf2d8acae1dccb438e
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 11:32:27 +0100

    create basic App strutcture

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import HeaderContainer from "./components/Header/Header-Container";
+import MainContainer from "./components/Main/Main-Container";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App-Container">
+      <HeaderContainer />
+      <MainContainer />
+    </div>
+  );
 }
 
 export default App;
