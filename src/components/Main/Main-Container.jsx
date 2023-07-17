d03508b42a07eb901eadea8ce3aa598dee9b6abd
JacvaSipt <steamcow12@hotmail.com>
2023-07-17 16:59:56 +0100

    refactor to use searchParams.get

--- src/components/Main/Main-Container.jsx
+++ src/components/Main/Main-Container.jsx
@@ -3,17 +3,22 @@ import Article from "./Article";
 import { getArticles } from "../utils/Api-Util";
 import { useParams } from "react-router-dom";
 import PageButtons from "./PageButtons";
+import { useSearchParams, Link } from "react-router-dom";
 
 function MainContainer(params) {
-  const { topics } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const [articles, setArticles] = useState([]);
   const [currentPageNumber, setCurrentPageNumber] = useState(1);
 
   useEffect(() => {
-    getArticles(currentPageNumber, topics).then((articleData) => {
-      setArticles(articleData);
-    });
-  }, [currentPageNumber, topics]);
+    const topicParam = searchParams.get("topics");
+    const sortByParam = searchParams.get("sortby");
+    getArticles(currentPageNumber, topicParam, sortByParam).then(
+      (articleData) => {
+        setArticles(articleData);
+      }
+    );
+  }, [currentPageNumber, searchParams]);
 
   const handlePageChange = (binary) => {
     binary
