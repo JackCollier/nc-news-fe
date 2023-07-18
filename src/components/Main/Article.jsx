8a801b646501b19f9979fd36e6ea749b6f0e55de
JacvaSipt <steamcow12@hotmail.com>
2023-07-18 10:50:48 +0100

    create logic to navigate to specific article via useNavigate

--- src/components/Main/Article.jsx
+++ src/components/Main/Article.jsx
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 function Article({ articles }) {
+  const navigate = useNavigate();
   return (
     <>
       {articles.map((article) => {
         return (
           <div className="article" key={article.article_id}>
             <section className="article-header">
-              <h2>{article.title}</h2>
+              <h2
+                onClick={() => {
+                  navigate(`/articles/:${article.article_id}`);
+                }}
+              >
+                {article.title}
+              </h2>
               <p>{article.author}</p>
             </section>
             <img src={article.article_img_url} alt="" />
