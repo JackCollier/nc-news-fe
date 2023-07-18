191220b1888d6a35a0b15fdc2e1448ddff4a180b
JacvaSipt <steamcow12@hotmail.com>
2023-07-18 11:20:24 +0100

    initiate api call and display basic page layout

--- src/components/Main/Article-By-Id.jsx
+++ src/components/Main/Article-By-Id.jsx
@@ -1,5 +1,43 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getArticleById } from "../utils/Api-Util";
+
 function ArticleById(params) {
-  return <h2>Hello</h2>;
+  const { articleid } = useParams();
+  const [individualArticle, setIndividualArticle] = useState([]);
+
+  useEffect(() => {
+    getArticleById(articleid).then((article) => {
+      setIndividualArticle(article);
+    });
+  }, []);
+
+  const {
+    title,
+    topic,
+    author,
+    body,
+    created_at,
+    votes,
+    article_img_url,
+    comment_count,
+  } = individualArticle;
+
+  return (
+    <div className="main-container">
+      <section className="article">
+        <h4>{topic}</h4>
+        <h4>{author}</h4>
+
+        <h2>{title}</h2>
+
+        <img src={article_img_url} alt="" />
+
+        <p>Votes: {votes}</p>
+        <p>comments: {comment_count}</p>
+      </section>
+    </div>
+  );
 }
 
 export default ArticleById;
