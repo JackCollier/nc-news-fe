1e07c5f7cf793e553bbcfbc884093b0ae6814cdc
JacvaSipt <steamcow12@hotmail.com>
2023-07-20 15:46:44 +0100

    create a route to handle non existant  addresses

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HeaderContainer from "./components/Header/Header-Container";
 import ArticleById from "./components/Main/Article-By-Id";
 import MainContainer from "./components/Main/Main-Container";
 import { Route, Routes } from "react-router-dom";
+import PageNotFound from "./components/Page-Not-Found";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainContainer />} />
         <Route path="/articles/:articleid" element={<ArticleById />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
