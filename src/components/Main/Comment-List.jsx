0a53b0dc8e61a2956e4133238c65b8c1468bab85
JacvaSipt <steamcow12@hotmail.com>
2023-07-20 12:20:08 +0100

    destructure article_id and comment_id for deletetion check

--- src/components/Main/Comment-List.jsx
+++ src/components/Main/Comment-List.jsx
@@ -1,16 +1,18 @@
 function CommentList({ articleComments }) {
-  return articleComments.map(({ author, body, created_at, votes }) => {
-    return (
-      <div className="comment">
-        <section className="article-header">
-          <h3>{author}</h3>
-          <p>{created_at}</p>
-        </section>
-        <p className="comment-body">{body}</p>
-        <p className="comment-vote">Votes: {votes}</p>
-      </div>
-    );
-  });
+  return articleComments.map(
+    ({ author, body, created_at, votes, article_id, comment_id }) => {
+      return (
+        <div className="comment">
+          <section className="article-header">
+            <h3>{author}</h3>
+            <p>{created_at}</p>
+          </section>
+          <p className="comment-body">{body}</p>
+          <p className="comment-vote">Votes: {votes}</p>
+        </div>
+      );
+    }
+  );
 }
 
 export default CommentList;
